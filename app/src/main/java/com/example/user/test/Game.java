c21777d8cc815ebab3c5688d4bddc1598bc8bbf2
Felix <felix.heuser@outlook.com>
2019-05-24 16:30:24 +0200

    Design for game and preGame activity

--- app/src/main/java/com/example/user/test/Game.java
+++ app/src/main/java/com/example/user/test/Game.java
@@ -6,24 +6,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Game extends Activity implements View.OnClickListener {
     GlobVar globVar = GlobVar.getInstance();
     Numbers numbers = Numbers.getInstance();
+    ImageView ivIcon;
     long startTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Grid layout buttons
         GridLayout mainLayout = findViewById(R.id.gridlayout);
         mainLayout.setColumnCount((int) Math.sqrt((double)GlobVar.getInstance().gameMode));
         mainLayout.setRowCount((int) Math.sqrt((double)GlobVar.getInstance().gameMode));
+
+        // Set correct answer
         TextView tv_gameShowCorrectAnswer = findViewById(R.id.tv_gameShowCorrectAnswer);
         String gameShowCorrectAnswer = "Find : "+numbers.getCorrectAnswer();
         tv_gameShowCorrectAnswer.setText(gameShowCorrectAnswer);
+
+        //Icon image view
+        ivIcon = findViewById(R.id.ivIcon);
+        ivIcon.setImageResource(R.drawable.switchsort_icon);
+        ivIcon.setOnClickListener(this);
+
         createButtons(mainLayout);
         startTime = System.currentTimeMillis();   // Start time for stop watch in milliseconds
     }
@@ -35,13 +47,16 @@ public class Game extends Activity implements View.OnClickListener {
             globVar.temp_text = numbers.getCorrectNumberType(temp_number);
 
             Button addButton =new Button(this);
+            addButton.setBackgroundResource(R.drawable.button_border);
+            addButton.setTextColor(getResources().getColor(R.color.text));
             addButton.setId(temp_number);
             addButton.setText(globVar.temp_text);
+
             addButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     checkCorrectAnswer(((Button) view).getText().toString());
                 }
-        });
+             });
             mainLayout.addView(addButton);
         }
     }
@@ -64,8 +79,12 @@ public class Game extends Activity implements View.OnClickListener {
         }
     }
 
-    @Override
     public void onClick(View v) {
+        switch (v.getId()) {
 
+            case R.id.ivIcon:
+                //TODO: Pause
+                break;
+        }
     }
 }
